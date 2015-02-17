13236153840768f2bba2b4899c85e7f33ff97ffc
Jonno Schmidt <jonno@schmidtfam.us>
2015-02-17 01:48:18 -0500

    added log for event type

--- web/routes/github.js
+++ web/routes/github.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var winston = require('winston');
 
 var github_helper = require('../helpers/github');
 
@@ -16,6 +17,8 @@ router.post('/webhook', function(req, res){
     // verify this?
 
     var deliveryID = req.header('X-Github-Delivery');
+
+    winston.info('New event from Github: ' + event);
 
     github_helper.handleEvent(event, req.body);
 
