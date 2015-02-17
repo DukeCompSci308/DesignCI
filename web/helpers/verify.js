cea15e0b4c2c18b5417f8eb908eca4ba418583f1
Jonno Schmidt <jonno@schmidtfam.us>
2015-02-17 01:47:27 -0500

    Added duke org verify

--- web/helpers/verify.js
+++ web/helpers/verify.js
@@ -1,36 +1,17 @@
 var express = require('express');
-var winston = require('winston');
+var winston = require('./logger');
 
-var github = function() {
-    var handleEvent  = function(event, data) {
-        // Handle the events we care about - https://developer.github.com/v3/activity/events/types
-        switch (event) {
-            case 'repository':
-                repositoryEvent(data);
-                break;
-            case 'commit_comment':
-            case 'fork':
-            case 'pull_request':
-                break;
-        }
-
-    };
-
-    /**
-     * Occurs when an event occurs on a repository. Currently only a create event.
-     * @param data The data that was sent for the event.
-     */
-    var repositoryEvent = function (data) {
-        if (data.action === "created") {
-            winston.info("New repo created: " + data.repository.name);
-            winston.info("Organization: " + data.organization.login);
-
-        }
-    };
+var verify = function() {
 
     return {
-        handleEvent: handleEvent
+        /**
+         * Verifies that an organization is part of Duke.
+         * @param name
+         */
+        organizationName: function(name) {
+            return name.indexOf('duke') >= 0 || name === 'baxterandthehackers';
+        }
     }
 };
 
-module.exports = github();
+module.exports = verify();
