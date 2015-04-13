152cbc7da60a9ea499faed8854af78bd7e55f2eb
Jonno Schmidt <jonno@schmidtfam.us>
2015-04-13 03:42:10 -0400

    updated index pag

--- web/routes/index.js
+++ web/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Duke Design', user: { name: 'Tadimsky'} });
 });
 
 module.exports = router;
