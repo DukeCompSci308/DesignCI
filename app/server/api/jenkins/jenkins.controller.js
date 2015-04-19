49adf15d4b00bd9f0f3f39ff48d4dcf0f6c6791a
Jonno Schmidt <jonno@schmidtfam.us>
2015-04-19 18:56:34 -0400

    Added status check to api endpoint

--- app/server/api/jenkins/jenkins.controller.js
+++ app/server/api/jenkins/jenkins.controller.js
@@ -5,6 +5,7 @@ var _ = require('lodash');
 var jenkins = require('../../helpers/jenkins');
 
 var buildAPIURL = function(semester, job) {
+  // hack to get around the jenkins api package
   var url = semester + '/' + job.project + '/' + encodeURIComponent(job.team);
   return url;
 };
@@ -17,11 +18,11 @@ exports.job = function(req, res) {
   var jenkinsJob = buildAPIURL(req.semesterName, req.jobURL);
   console.log(jenkinsJob);
   jenkins.api.job.exists(jenkinsJob, function(err, exists) {
-    if (err) throw err;
-    console.log('exists', exists);
+    if (err || !exists) {
+      res.status(404).json({msg: 'No job with that name.'});
+    }
+    res.json({semester: req.semesterName, job: req.jobURL});
   });
-
-  res.json({semester: req.semesterName, job: req.jobURL});
 };
 
 exports.jobParse = function(req,res,next,job) {
