074cba1719032e3ccbdbbe100df1a86818d1c2ab
Jonno Schmidt <jonno@schmidtfam.us>
2015-04-20 02:28:08 -0400

    duplicate code!

--- app/client/app/duplicateCode/duplicateCode.directive.js
+++ app/client/app/duplicateCode/duplicateCode.directive.js
@@ -4,8 +4,12 @@ angular.module('appApp')
   .directive('duplicateCode', function () {
     return {
       templateUrl: 'app/duplicateCode/duplicateCode.html',
+      scope: {
+        metrics: '=metrics'
+      },
       restrict: 'EA',
       link: function (scope, element, attrs) {
+
       }
     };
   });
--- app/client/app/project/project.controller.js
+++ app/client/app/project/project.controller.js
@@ -4,9 +4,15 @@ angular.module('appApp')
   .controller('ProjectCtrl', function ($scope, $stateParams, $http) {
     $scope.message = 'Hello';
     $scope.projectName = $stateParams.projectName;
+    $scope.metrics = {};
 
     $http.get('/api/jenkins/' + $stateParams.projectName).success(function(data) {
       $scope.project = data;
+      console.log(data);
+    });
+
+    $http.get('/api/jenkins/' + $stateParams.projectName + '/metrics').success(function(data) {
+      $scope.metrics.jenkins = data;
       console.log(data);
     });
   });
