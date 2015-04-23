8091ccc90ac8230b111e0038fb9aa2d4d490f00a
Jonno Schmidt <jonno@schmidtfam.us>
2015-04-23 16:36:24 -0400

    more stuff to get metrics from sonarqube

--- app/server/helpers/sonarqube.js
+++ app/server/helpers/sonarqube.js
@@ -9,19 +9,53 @@ var request = require('request');
 
 var sonarqube = function() {
   var api = request.defaults({
-    baseUrl: config.sonarqube.server + '/api',
+    baseUrl: "http://" + config.sonarqube.server + '/api',
     auth : {
       user: config.sonarqube.username,
       password: config.sonarqube.password
-    }
+    },
+    headers: [
+      {
+        name: 'content-type',
+        value: 'application/json'
+      }
+    ]
   });
 
-  api.get('/languages/list', function(error, response, body) {
-    console.log(body);
-  });
+  /* Important endpoints:
+      - api/issues/search
+      - api/resources/index
+   */
+
+  // http://design.cs.duke.edu/sonar/api/resources/index?resource=duke-compsci308-spring2015.voogasalad_ScrollingDeep:voogasalad_ScrollingDeep&metrics=class_complexity,function_complexity,public_documented_api_density,violations,blocker_violations,critical_violations,ncloc,accessors,public_api
+
+
+
 
   return {
+    metrics: function(project, callback) {
+      api.get(
+        {
+          url: '/resources/index',
+          qs: {
+            resource: project
 
+            //metrics: ['class_complexity','function_complexity','public_documented_api_density','violations','blocker_violations','critical_violations','ncloc','accessors','public_api']
+          }
+      },
+        function(error, response, body) {
+          callback(error, JSON.parse(body)[0]);
+      });
+    },
+    test: function() {
+      api.get('/languages/list', function(error, response, body) {
+        if (error) {
+        }
+        console.log(body);
+        console.log(response);
+        console.log(error);
+      });
+    }
   };
 };
 
