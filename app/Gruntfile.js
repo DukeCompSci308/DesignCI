d57943d6a5fbad81cef0134107ac3b89718f525f
Jonno Schmidt <jonno@schmidtfam.us>
2015-04-23 13:52:28 -0400

    Removed open from grunt

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -49,11 +49,12 @@ module.exports = function (grunt) {
         }
       }
     },
-    open: {
-      server: {
-        url: 'http://localhost:<%= express.options.port %>'
-      }
-    },
+    //},
+    //open: {
+    //  server: {
+    //    url: 'http://localhost:<%= express.options.port %>'
+    //  }
+    //},
     watch: {
       injectJS: {
         files: [
@@ -542,7 +543,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('serve', function (target) {
     if (target === 'dist') {
-      return grunt.task.run(['build', 'env:all', 'env:prod', 'express:prod', 'wait', 'open', 'express-keepalive']);
+      return grunt.task.run(['build', 'env:all', 'env:prod', 'express:prod', 'wait', 'express-keepalive']);
     }
 
     if (target === 'debug') {
@@ -566,7 +567,6 @@ module.exports = function (grunt) {
       'autoprefixer',
       'express:dev',
       'wait',
-      'open',
       'watch'
     ]);
   });
